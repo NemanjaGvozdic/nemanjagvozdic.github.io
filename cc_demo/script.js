7bc1df945e93e0a3c64a098b65bb3425af793217
Nemanja Gvozdic <NemanjaGvozdic@users.noreply.github.com>
2018-10-21 19:15:58 +0200

    git commit

---
+++ cc_demo/script.js
@@ -0,0 +1,11 @@
+var changeBtn = document.querySelector(".change");
+
+function change(){
+    if( changeBtn.textContent == "front"){
+        changeBtn.textContent="back";
+    }else{
+        changeBtn.textContent="front";
+    }
+}
+
+//POPRAVI OVO!!!
---
+++ website-3/script.js
@@ -0,0 +1,26 @@
+const item1 = document.querySelector("#item1");
+const item2 = document.querySelector("#item2");
+const item3 = document.querySelector("#item3");
+
+
+
+item1.style.display = "none";
+item2.style.display = "none";
+item3.style.display = "none";
+
+function change(change) {
+    change.classList.toggle("close");
+    if(change.classList.contains("close")){
+        item1.style.display = "block";
+        item2.style.display = "block";
+        item3.style.display = "block";
+
+    } else{
+        item1.style.display = "none";
+        item2.style.display = "none";
+        item3.style.display = "none";
+
+    }
+}
+
+
