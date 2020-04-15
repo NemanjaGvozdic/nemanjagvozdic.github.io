c8856b06775240a540bdf2cc1dc36ced95d51df3
NemanjaGvozdic <39676423+NemanjaGvozdic@users.noreply.github.com>
2020-04-15 02:50:49 +0200

    Add files via upload

--- sa-landing-page/src/index.js
+++ sa-landing-page/src/index.js
@@ -1,3 +1,6 @@
+window.scrollTo(0, 0);
+
+
 var randomNum = Math.floor(Math.random() * 4000 + 3000);
 setTimeout(hideLoader, randomNum);
 setTimeout(saLogo, randomNum + 300);
@@ -6,9 +9,13 @@ var showBody = document.getElementById("body");
 var loader = document.getElementById("loader");
 var loaderLogo = document.getElementById("loader-logo");
 var loadingCircle = document.getElementById("loading-circle");
+
 var logoNoAnim = document.getElementById("sa-logo");
 var welcomeText = document.getElementById("welcome-text");
+var welcomeBtns = document.getElementById("welcome-btns");
 var arrow = document.getElementById("arrow");
+var welcomeContent = document.getElementById("welcome-content");
+
 
 function hideLoader() {
   showBody.classList.add("show-scroll");
@@ -24,7 +31,10 @@ function saLogo() {
   function saLogoGore() {
     logoNoAnim.classList.add("sa-logo-up");
     welcomeText.classList.add("welcome-text-active");
-    arrow.classList.add("arrow-active");
+   //welcomeBtns.classList.add("welcome-btns-active");
+    //arrow.classList.add("arrow-active");
+
+    welcomeContent.classList.add("welcome-content-active");
   }
 }
 
@@ -35,14 +45,23 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "0 20px";
     document.getElementById("logo").style.width = "130px";
-    document.getElementById("logo").style.margin = "5px 150px";
-    document.getElementById("nav-last").style.margin = "25px 300px 0 20px";
-    document.getElementById("navbar").style.boxShadow = "0px 0px 10px 1px #000"; 
+    document.getElementById("logo").style.margin = "5px 300px";
+    document.getElementById("nav-last").style.margin = "0 300px 0 20px";
   } else {
     document.getElementById("navbar").style.padding = "3px 20px";
     document.getElementById("logo").style.width = "150px";
-    document.getElementById("logo").style.margin = "5px 50px";
-    document.getElementById("nav-last").style.margin = "25px 150px 0 20px";
-    document.getElementById("navbar").style.boxShadow = "0 0 0 transparent"; 
+    document.getElementById("logo").style.margin = "5px 150px";
+    document.getElementById("nav-last").style.margin = "0 150px 0 20px";
   }
 } 
+
+function runTs() {
+  var nicknameTsTxtbox = document.getElementById("nickname-ts-txtbox").value;
+
+  var openTs = window.open(`ts3server://ts3.sa-rpg.com/?nickname=${ nicknameTsTxtbox }`);
+  openTs.close();
+}
+
+
+// IZBACI ELEMENTE KOJI SE VISE NE KORISTE ZA ANIMACIJE
+// IZ JS-A KAO I IZ HTML-A
